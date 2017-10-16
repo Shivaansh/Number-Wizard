baee0d7d9fbc717baeddac5e54e4c7c3fa23c3bf
Shivaansh Prasann <shivaanshseal@gmail.com>
2017-10-16 15:12:21 -0400

    Add files via upload

---
+++ LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public void LoadLevel(string name){//public because it needs to be called outside the class as well
+		Debug.Log("Level Load requested for: " + name);
+		Application.LoadLevel (name);
+	}
+	public void QuitRequest(){
+		Debug.Log("I want to quit!");
+		Application.Quit ();
+	}
+
+}
---
+++ NumberWizard.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess = 1000;
+	int MaxGuessesAllowed = 12;
+	public Text message;
+	public Text guessCount;
+	
+	
+	
+
+
+	// Use this for initialization
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame(){
+		max = 3000;
+		min = 1;
+		max = max+1;
+		
+		
+
+		}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)){
+			//print("Up arrow was pressed");
+			GuessHigher ();
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow)){
+			//print("Down arrow was pressed");
+			GuessLower ();
+		}	
+		//else if (Input.GetKeyDown(KeyCode.Return)){
+			//print("I Won");
+			//StartGame ();
+		//}
+	}
+	
+	public void GuessHigher(){
+		min = guess;
+		NextGuess ();
+	
+	}
+	
+	public void GuessLower(){
+		max = guess;
+		NextGuess ();
+		
+	}
+	
+	void NextGuess(){
+		guess = (max+min)/2;
+		message.text = guess.ToString ();
+		MaxGuessesAllowed = MaxGuessesAllowed-1;
+		guessCount.text = MaxGuessesAllowed.ToString ();
+		if(MaxGuessesAllowed==0){
+			Application.LoadLevel ("Win");
+		}
+
+	}
+}
